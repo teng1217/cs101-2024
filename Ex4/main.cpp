f90f60201ec09a56af04682851310e0c0e672828
Brian H <84963794+teng1217@users.noreply.github.com>
2024-04-18 14:58:31 +0800

    Create main.cpp

---
+++ Ex4/main.cpp
@@ -0,0 +1,14 @@
+class Car {
+public:
+string m_brand; string m_model;
+int m_year;
+int m_MaxSeating;
+Car (string x, string y, int z, int s) {
+m_brand = X;
+m_model = y;
+m_year = z;
+m_MaxSeating = S;
+}
+int get_MaxSeating() {
+return m_MaxSeating;
+成員函式
