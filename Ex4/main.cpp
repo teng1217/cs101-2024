43c76eaf652b7bfb7b6b4951a62acb7a3384418d
Brian H <84963794+teng1217@users.noreply.github.com>
2024-05-09 13:39:53 +0800

    Update main.cpp

--- Ex4/main.cpp
+++ Ex4/main.cpp
@@ -1,33 +1,80 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Car {
-private:
-Engine m_Engine;
-Fueltank m_Fueltank;
-int m_MaxSeating;
-int m_price; int m_base;
-string m_brand; string m_model;
-int m_year;
-void m_UpdatePrice(int base =500000) {}
-Car(string x, string y,
-int z, int s)
-int get_MaxSeating() ti
-int get_Price() {C} void set_base(int n) {}
-string get_brand()
-bool get_EngineStatus()
-bool startEngine()
-bool stopEngineo
-int get_Gas_grade()
-int set_Gas_grade(int gas 98)
-{}
-int fuel_up(int v, int gas 98)
-{}
+    protected:
+    string m_DriveMode;
+    private:
+    int m_MaxSeating;
+    int m_price;
+    void m_UpdatePrice(int base=500000){
+        m_price=m_MaxSeating*base;
+    }
+    public:
+    string m_brand;
+    string m_model;
+    int m_year;
+
+    Car(string x,string y,int z,int s){
+        m_brand=x;
+        m_model=y;
+        m_year=z;
+        m_MaxSeating=s;
+        m_UpdatePrice();
+        m_DriveMode="No-wheel";
+    }
+    int get_MaxSeatint(){
+        return m_MaxSeating;
+    }
+    int get_Price(){
+        return m_price;
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+    
 };
-class Fueltank {
-private:
-int m_FueltankCapacity;
-int m_Gas_grade;
-public:
-FueltankCint FueltankCapacity=3000, int Gas-98) {3
-int fuel_up(int v, int gas) {0); int set_Gas_grade(int Gas_grade) {c
-•
+class BMW_Car : public Car{
+
+    public:
+    BMW_Car(string y,int z,int s):Car("BMW",y,z,s){
+        cout<<"Constructing BMW_Car\n";
+        m_DriveMode="Rear-wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
 };
-int get_Gas_grade) {c];
+class AUDI_Car : public Car{
+
+    public:
+    AUDI_Car(string y,int z,int s):Car("Audi",y,z,s){
+        cout<<"Constructing AUDI_Car\n";
+        m_DriveMode="Front-wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+};
+class BENZ_Car : public Car{
+
+    public:
+    BENZ_Car(string y,int z,int s):Car("Benz",y,z,s){
+        cout<<"Constructing BENZ_Car\n";
+        m_DriveMode="Front-wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+};
+    
+int main(){
+    BMW_Car car_1("X5",2023,6);
+    cout<<car_1.m_brand<<" : Max Seating = "<<car_1.get_DriveMode()<<endl;
+    AUDI_Car car_2("A1",2021,2);
+    cout<<car_2.m_brand<<" : Max Seating = "<<car_2.get_DriveMode()<<endl;
+    BENZ_Car car_3("C-class",2022,5);
+    cout<<car_3.m_brand<<" : Max Seating = "<<car_3.get_DriveMode()<<endl;
+
+}
