bae1a554658340a648ac55ccbe71ac07f5b7ce4d
Brian H <84963794+teng1217@users.noreply.github.com>
2024-05-09 13:34:26 +0800

    Update main.cpp

--- Ex4/main.cpp
+++ Ex4/main.cpp
@@ -1,14 +1,33 @@
 class Car {
-public:
+private:
+Engine m_Engine;
+Fueltank m_Fueltank;
+int m_MaxSeating;
+int m_price; int m_base;
 string m_brand; string m_model;
 int m_year;
-int m_MaxSeating;
-Car (string x, string y, int z, int s) {
-m_brand = X;
-m_model = y;
-m_year = z;
-m_MaxSeating = S;
-}
-int get_MaxSeating() {
-return m_MaxSeating;
-成員函式
+void m_UpdatePrice(int base =500000) {}
+Car(string x, string y,
+int z, int s)
+int get_MaxSeating() ti
+int get_Price() {C} void set_base(int n) {}
+string get_brand()
+bool get_EngineStatus()
+bool startEngine()
+bool stopEngineo
+int get_Gas_grade()
+int set_Gas_grade(int gas 98)
+{}
+int fuel_up(int v, int gas 98)
+{}
+};
+class Fueltank {
+private:
+int m_FueltankCapacity;
+int m_Gas_grade;
+public:
+FueltankCint FueltankCapacity=3000, int Gas-98) {3
+int fuel_up(int v, int gas) {0); int set_Gas_grade(int Gas_grade) {c
+•
+};
+int get_Gas_grade) {c];
